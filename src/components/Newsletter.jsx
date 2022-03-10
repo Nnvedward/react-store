d75322ab66e3e2c6e3ede89b6501285ae6b23010
Nnvedward <nnvedward@gmail.com>
2022-03-10 15:56:58 +0100

    Setup newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,66 @@
+import {  faPaperPlane } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 60vh;
+    background-color: #fcf1ed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+const Title = styled.h1`
+    font-size: 70px;
+    margin-bottom: 20px;
+`
+
+
+const Description = styled.p`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+`
+
+
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+`
+
+
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+`
+
+
+const Button = styled.button`
+    flex: 0.5;
+    border: none;
+    background-color: black;
+    color: white;
+    cursor: pointer;
+`
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title>Newsletter</Title>
+        <Description>Get timely updates from your favorite products.</Description>
+        <InputContainer>
+            <Input placeholder="Your email"/>
+            <Button>
+                <FontAwesomeIcon icon={faPaperPlane} />
+            </Button>
+        </InputContainer>
+    </Container>
+  )
+}
+
+export default Newsletter
